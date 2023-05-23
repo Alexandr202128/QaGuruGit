8485351c5052210dc31abe5c6cf61fce5573730c
Alexandr Solovyov <18solovjov@gmail.com>
2023-05-23 21:34:06 +0300

    init branch new_test

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -1,8 +1,9 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BaseTests {
     @Test
     public void check() {
-        
+        Assertions.assertEquals(2, 2);
     }
 }
